fb2a10fe84351fe8536b734c9136ce4f8144f5de
zedfaceless <zedfaces.fernandez@gmail.com>
2025-09-07 06:59:47 +0800

    new update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
-const professionalRoutes = require('./routes/professional');
+const professionalRoutes = require('./routes/router');
 
 const port = process.env.PORT || 8080;
 const app = express();
