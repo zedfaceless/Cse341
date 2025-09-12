59ce4420d2f37943afb3d6514c7206a40328535c
zedfaceless <zedfaces.fernandez@gmail.com>
2025-09-12 14:05:44 +0800

    updated swagger file and server.js to include swagger-ui-express package

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const professionalRoutes = require('./router/router');
-const swaggerUI = require('swagger-ui-express');
+const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
-const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app
   .use(bodyParser.json())
